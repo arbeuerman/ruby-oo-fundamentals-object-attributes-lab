e9c3c5dcf6fffe009e25aecc9c71553c155b19cc
Alexandra <arbeuerman@gmail.com>
2021-03-15 18:26:50 -0500

    add getter and setter mathods to classes

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Dog
+    
+    #getter functions
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end 
+
+    #setter functions
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end 
+end
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+
+
+class Person
+
+  #getter methods 
+  def name
+    @name
+  end 
+
+  def job
+    @job
+  end 
+
+  #setter methods
+  def name=(name)
+    @name = name
+  end
+
+  def job=(job)
+    @job = job
+  end 
+
+end 
